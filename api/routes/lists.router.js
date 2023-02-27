362ae92c58c41a354f6477739ea77e1290be5f43
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-02-27 14:39:12 +0000

    Router connection done

---
+++ api/routes/lists.router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+
+const {
+  getProjectLists
+  // getListById,
+  // createList,
+  // updateList,
+  // deleteListById
+} = require('../controllers/lists.controller')
+
+// router.get('/', getProjectLists)
+// router.get('/:id', getListById)
+// router.post('/', createList)
+// router.put('/:id', updateList)
+// router.delete('/:id', deleteListById)
+
+module.exports = router
--- api/routes/router.js
+++ api/routes/router.js
@@ -4,12 +4,14 @@ const authRouter = require('./auth.router')
 const usersRouter = require('./users.router')
 const projectsRouter = require('./projects.router')
 const sprintsRouter = require('./sprints.router')
+const listsRouter = require('./lists.router')
 const { authUser } = require('../utils') // Authenticated Route
 
 router.use('/auth', authRouter)
 router.use('/users', usersRouter)
 router.use('/projects', authUser, projectsRouter)
 router.use('/sprints', authUser, sprintsRouter)
+router.use('/lists', authUser, listsRouter)
 
 router.get('/profile', authUser, (req, res) => {
   res.json(res.locals.user)
