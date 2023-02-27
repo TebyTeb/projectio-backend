be2f5ef55d496a115bab0a4855fbd8190032a28a
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-02-27 15:06:19 +0000

    Routing complete

--- api/routes/router.js
+++ api/routes/router.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users.router')
 const projectsRouter = require('./projects.router')
 const sprintsRouter = require('./sprints.router')
 const listsRouter = require('./lists.router')
+const tasksRouter = require('./tasks.router')
 const { authUser } = require('../utils') // Authenticated Route
 
 router.use('/auth', authRouter)
@@ -12,6 +13,7 @@ router.use('/users', usersRouter)
 router.use('/projects', authUser, projectsRouter)
 router.use('/sprints', authUser, sprintsRouter)
 router.use('/lists', authUser, listsRouter)
+router.use('/tasks', authUser, tasksRouter)
 
 router.get('/profile', authUser, (req, res) => {
   res.json(res.locals.user)
---
+++ api/routes/tasks.router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+
+const {
+  getSprintTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTaskById
+} = require('../controllers/tasks.controller')
+
+// router.get('/', getSprintTasks)
+// router.get('/:id', getTaskById)
+// router.post('/', createTask)
+// router.put('/:id', updateTask)
+// router.delete('/:id', deleteTaskById)
+
+module.exports = router
