2a152c26fded443eb7b2ab16cfeea73711217cfd
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-02-27 14:55:44 +0000

    Lists CRUD done

--- api/routes/lists.router.js
+++ api/routes/lists.router.js
@@ -1,17 +1,17 @@
 const router = require('express').Router()
 
 const {
-  getProjectLists
-  // getListById,
-  // createList,
-  // updateList,
-  // deleteListById
+  getProjectLists,
+  getListById,
+  createList,
+  updateList,
+  deleteListById
 } = require('../controllers/lists.controller')
 
-// router.get('/', getProjectLists)
-// router.get('/:id', getListById)
-// router.post('/', createList)
-// router.put('/:id', updateList)
-// router.delete('/:id', deleteListById)
+router.get('/', getProjectLists)
+router.get('/:id', getListById)
+router.post('/', createList)
+router.put('/:id', updateList)
+router.delete('/:id', deleteListById)
 
 module.exports = router
